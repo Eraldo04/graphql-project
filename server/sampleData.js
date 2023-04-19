46f0dad8d01a81d6811db9aea70a516b77e10ec6
Eraldo Kanani <eraldokanani@dotbalkans.onmicrosoft.com>
2023-04-19 14:15:40 +0200

    first commit

---
+++ server/sampleData.js
@@ -0,0 +1,48 @@
+const projects = [
+    {
+        id: '1',
+        clientId: '1',
+        name: 'website',
+        description : 'Website ajadsfjaksf asflfjaslajsfasljvmncqdjasflj asjfasfljasfjlafjal',
+        status : 'In Progress',
+    },
+    {
+        id: '2',
+        clientId: '2',
+        name: 'web',
+        description : 'Web ajadsfjaksf asflfjaslajsfasljvmncqdjasflj asjfasfljasfjlafjal',
+        status : 'In Progress',
+    },
+    {
+        id: '3',
+        clientId: '3',
+        name: 'App',
+        description : 'App ajadsfjaksf asflfjaslajsfasljvmncqdjasflj asjfasfljasfjlafjal',
+        status : 'In Progress',
+    }
+];
+
+//Clients
+
+const clients = [
+    {
+        id: 1,
+        name: 'Eraldo Kanani',
+        email: 'ekanani@gmail.com',
+        phone: '68-55-67-855',
+    },
+    {
+        id: 2,
+        name: 'Mart Gjepali',
+        email: 'mgjepali@gmail.com',
+        phone: '69-45-69-851',
+    },
+    {
+        id: 3,
+        name: 'Donald Zhuku',
+        email: 'dzhuku@gmail.com',
+        phone: '66-48-69-341',
+    },
+]
+
+module.exports = { projects, clients };
