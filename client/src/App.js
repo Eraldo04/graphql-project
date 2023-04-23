ac1facd27ec32d2358142c423bb718bc63e2c999
eraldo04 <eraldokanani2@gmail.com>
2023-04-23 12:20:42 +0200

    sd-3: client frontend setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import TopHeader from "./components/TopHeader";
+
+function App() {
+  return (
+    <>
+    <TopHeader />
+    <div className="container">
+      <h1>Hello There</h1>
+    </div>
+    </>
+  );
+}
+
+
+export default App;
